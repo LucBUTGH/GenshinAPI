82c256255437e457046b4c6c564b5ca8a7ca3fca
LucBUTGH <lucleveque14@outlook.fr>
2024-01-22 01:00:39 +0100

    fix: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 </body>
 </html>
-<footer class="bg-gray-800 text-white p-4 mt-5">
+<footer class="mt-auto bg-gray-800 text-white p-4 mt-5">
     <div class="container mx-autophp">
         <!-- Contenu du footer -->
         <p> &copy; 2024 Luc Leveque. Tous droits réservés.</p>
